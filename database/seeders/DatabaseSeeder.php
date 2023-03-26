ce54d2ecc4ccd0cf644c4f6462962fbc8367eda2
Andres Sosa <ing.asosam@gmail.com>
2023-03-26 10:46:39 -0600

    chore: Add seeders for dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Author::factory()->count(5)->create();
+        Book::factory()->count(15)->create();
     }
 }
